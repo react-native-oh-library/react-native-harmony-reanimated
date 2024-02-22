78f356920c432c9659a87b1bc469de693744917c
Louis-C7 <chenlixi1@huawei.com>
2024-02-22 15:19:06 +0800

    add RNGH support

--- tester/harmony/entry/src/main/cpp/PackageProvider.cpp
+++ tester/harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -1,9 +1,10 @@
 #include "RNOH/PackageProvider.h"
-#include "ReanimatedPackage.h"
+#include "generated/RNOHGeneratedPackage.h"
 #include "GestureHandlerPackage.h"
+#include "ReanimatedPackage.h"
 
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
-    return {std::make_shared<ReanimatedPackage>(ctx), std::make_shared<GestureHandlerPackage>(ctx)};
+    return {std::make_shared<RNOHGeneratedPackage>(ctx), std::make_shared<GestureHandlerPackage>(ctx), std::make_shared<ReanimatedPackage>(ctx),};
 }
