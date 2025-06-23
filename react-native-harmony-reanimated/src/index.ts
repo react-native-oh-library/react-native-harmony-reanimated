646d9034f02419096ec034891c56a1fc84f8e1d3
quguiren <quguiren1@huawei-partners.com>
2025-06-23 16:08:09 +0800

    update reanimated_3.18.0

--- react-native-harmony-reanimated/src/index.ts
+++ react-native-harmony-reanimated/src/index.ts
@@ -1,9 +1,6 @@
-'use strict'
+'use strict';
 
-import * as Animated from 'react-native-reanimated/src/Animated';
-export * from './reanimated2/NativeReanimated/NativeReanimated'
-export * from 'react-native-reanimated/src/reanimated2';
-
-global._REANIMATED_VERSION_JS = "3.6.0";
+import Animated from 'react-native-reanimated';
+export * from 'react-native-reanimated';
 
 export default Animated;
