7fa1e16d51af65939f8a6301cca9b62b7fe1f924
tyBrave <tianyong21@h-partners.com>
2025-12-25 21:08:11 +0800

    fix:修复Modal与Aimated的组件在同一层级上时，Modal弹窗不生效
    Signed-off-by: tyBrave <tianyong21@h-partners.com>

--- tester/harmony/reanimated/src/main/cpp/Common/cpp/Fabric/ShadowTreeCloner.h
+++ tester/harmony/reanimated/src/main/cpp/Common/cpp/Fabric/ShadowTreeCloner.h
@@ -12,10 +12,18 @@ using namespace react;
 
 namespace reanimated {
 
+using PropsMap = std::unordered_map<const facebook::react::ShadowNodeFamily*, folly::dynamic>;
+using ChildrenMap =
+    std::unordered_map<const ShadowNodeFamily *, std::unordered_set<int>>;
+
 ShadowNode::Unshared cloneShadowTreeWithNewProps(
     const ShadowNode::Shared &oldRootNode,
     const ShadowNodeFamily &family,
     RawProps &&rawProps);
+
+RootShadowNode::Unshared cloneShadowTreeWithNewPropsUnmounted(
+    RootShadowNode::Unshared const &oldRootShadowNode,
+    const PropsMap &propsMap);
 
 } // namespace reanimated
 
